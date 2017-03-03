66b0920c238c000ab77a8f4cd3dae134c462d6d5
Cuong Pham <anget1507@live.com>
2017-03-03 10:58:04 +0700

    Add project files.

---
+++ LargestGap/IAisleLayoutProvider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LargestGap
+{
+    public interface IAisleLayoutProvider
+    {
+        double GetDistanceForTopRoute(Item item1, Item item2);
+        double GetDistanceForBottomRoute(Item item1, Item item2);
+        List<Item> GetSameRouteAisle(List<Item> items, int aisle);
+        List<Item> GetNextAisle(List<Item> sortedList, int aisle);
+    }
+
+    public class AisleLayoutProvider : IAisleLayoutProvider
+    {
+        public double GetDistanceForTopRoute(Item lastItemOfFirstAisle, Item nextBin)
+        {
+            return Program.Bin - lastItemOfFirstAisle.Bin + (Program.Bin - nextBin.Bin) +
+                   (nextBin.Aisle - lastItemOfFirstAisle.Aisle);
+        }
+
+        public double GetDistanceForBottomRoute(Item lastItemOfFirstAisle, Item nextBin)
+        {
+            return lastItemOfFirstAisle.Bin + nextBin.Bin + (nextBin.Aisle - lastItemOfFirstAisle.Aisle);
+        }
+
+        public List<Item> GetSameRouteAisle(List<Item> items, int aisle)
+        {
+            if (aisle % 2 != 0)
+                return null;
+            return items.Where(e => e.Aisle == aisle + 1).ToList();
+        }
+        public List<Item> GetNextAisle(List<Item> sortedList, int aisle)
+        {
+            var listAisles = sortedList.Where(e => e.Aisle > aisle).OrderBy(e => e.Aisle);
+            return listAisles.Where(e => e.Aisle == (listAisles.FirstOrDefault()?.Aisle ?? aisle)).OrderBy(e => e.Bin).ToList();
+        }
+    }
+}
